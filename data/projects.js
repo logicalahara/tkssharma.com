92866f3a74b13270292f7f82db3deed4d27c658c
tkssharma <tarun.softengg@gmail.com>
2020-03-07 09:20:00 +0530

    links updated

--- data/projects.js
+++ data/projects.js
@@ -2,42 +2,42 @@ export default [
   {
     icon: '🏺',
     title: 'GenNexTraining',
-    path: 'https://tkssharma.herokuapp.com',
+    path: 'https://training.tkssharma.com',
     source: 'https://github.com/tkssharma/GennextTrainingWeb',
     description: 'A web-based training portal to provide learning resources',
   },
   {
     icon: '🌙',
     title: 'My Profile',
-    path: 'https://tkssharma.github.io/new-moon/',
+    path: 'https://tkssharma.com',
     source: 'https://github.com/tkssharma.com/',
     description: 'The optimized dark theme for web development. Your new favorite theme (gatsby profile app)',
   },
   {
     icon: '🎮',
     title: 'Code Mentor',
-    path: 'https://tkssharma.github.io/',
+    path: 'https://hackio.tkssharma.com/',
     source: 'https://github.com/tkssharma/code-mentor',
     description: 'A Code mentor app for exploring learning sessions',
   },
   {
     icon: '🔱',
     title: 'Code Lab',
-    path: 'https://tkssharma.github.io',
+    path: 'https://academy.tkssharma.com/',
     source: 'https://github.com/tkssharma/code-lab-app',
     description: 'A simple Code Academy Application as Learning solution for developers and expers',
   },
   {
     icon: '🏺',
     title: 'Nanny Finder',
-    path: 'https://tkssharma.github.io',
+    path: 'https://nanny.tkssharma.com/',
     source: 'https://github.com/tkssharma/nanny-Finder-App',
     description: 'nanny Finder App to find hose worker around',
   },
   {
     icon: '⛏️',
     title: ' ubmas',
-    path: 'https://tkssharma.github.io/',
+    path: 'https://ubmas.tkssharma.com/',
     source: 'https://github.com/tkssharma/ubmas-app',
     description:
       'UBMAS Application for traverlers to find local food wherever they travel (Find local food and become guest)',
@@ -45,7 +45,7 @@ export default [
   {
     icon: '🐍',
     title: 'blabla Scooty',
-    source: 'https://github.com/tkssharma/',
+    source: 'https://scooty.tkssharma.com/',
     description: 'Scooty rental platform for travellers to find scooty on rentals',
   }
 ]
